a6f13f76cd16ee801b6006ce069c3ccfef5474e7
Midfield-mmm <enfant.maison52@gmail.com>
2026-02-27 10:35:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "mgame-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js"
+];
+
+// Installation
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activation
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Interception des requêtes
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request)
+          .then(fetchResponse => {
+            return caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          })
+          .catch(() => {
+            return caches.match("/index.html");
+          });
+      })
+  );
+});
